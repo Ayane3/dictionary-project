a1245ccb8c95f323a72a78d134fd938785355ec6
Ayane3 <ayanemanuel@gmail.com>
2022-06-29 14:57:59 -0400

    Updated the Pexels Api URL

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -26,12 +26,8 @@ export default function Dictionary(props) {
     let pexelsApiKey =
       "563492ad6f917000010000013cd8ef7496444837a91977ec2071beac";
     let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
-
-    axios
-      .get(pexelsApiUrl, {
-        headers: { Authorization: `Bearer ${pexelsApiKey}` },
-      })
-      .then(handlePexelsResponse);
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
