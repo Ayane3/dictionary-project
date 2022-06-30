68937657249ecba2b8452ba721ace788a065f63f
Ayane3 <ayanemanuel@gmail.com>
2022-06-30 00:01:18 -0400

    Updated the background color

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.png";
 import Dictionary from "./Dictionary";
 
 import "./App.css";
@@ -8,21 +7,29 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <h1>Dictionary</h1>
         </header>
         <main>
-          <Dictionary defaultKeyword="sunset" />
+          <Dictionary defaultKeyword="earth" />
         </main>
         <footer className="App-footer">
           <small>
             {" "}
-            Coded by Ayane Manuel and is {""}
+            Coded by Ayane Manuel and is open-sourced on {""}
             <a
               href="https://github.com/Ayane3/dictionary-project"
               target="_blank"
               rel="noreferrer"
             >
-              open-sourced on GitHub
+              GitHub {""}
+            </a>
+            and hosted on {""}
+            <a
+              href="https://vocal-salamander-c45526.netlify.app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Netlify
             </a>
           </small>
         </footer>
